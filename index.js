bad9e11eaa0ffa961a3b4232e43826f22796e821
Chance Smith <chancesmith97@hotmail.com>
2020-10-01 01:38:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ class Formatter {
   }
 
   static sanitize(string){
-    return string.replace(/[&\/\\#,+()$~%.":*?<>{}]/g, '');
+    return string.replace(/[&\/\\#,+()$~%.":*?<>{}!]/g, '');
   }
 }
